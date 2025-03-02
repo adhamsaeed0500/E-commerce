619d532c6e0e22b4352190a331627197773191f9
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-03-02 16:51:45 +0200

    Create category with custom validation

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -19,11 +19,25 @@ namespace E_commerce_System.Controllers
         }
 
 
-            public IActionResult Create() 
+        public IActionResult Create() 
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
 
-
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
