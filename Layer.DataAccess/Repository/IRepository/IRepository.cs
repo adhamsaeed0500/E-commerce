f2c26fe36f4b77bea4121b12d768dfd5d4166262
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-03-16 22:00:21 +0200

    add generic IRepository Interface

---
+++ Layer.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace E_commerce_System.Layer.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
