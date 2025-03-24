84e5dd64c1086c04883599a758e6aa62575fc18d
Adham Saeed <adhamsaeeed0500@gmail.com>
2025-03-24 02:22:42 +0200

    add CustomerProduct

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using E_commerce_System.Layer.DataAccess.Repository.IRepository;
+using E_commerce_System.Layer.Models;
 using Layer.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -6,17 +8,27 @@ namespace E_commerce_System.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
-    {   
+    {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        
+         public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+         {
             _logger = logger;
-        }
+            _unitOfWork = unitOfWork;
+         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == id, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
