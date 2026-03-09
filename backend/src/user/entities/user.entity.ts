d1f7198aae2c909b5734f88be1324730dde87aba
Adham ElSaid <adhamsaeed0500@gmail.com>
2026-03-09 08:28:38 +0200

    add user entity

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ unique: true, length: 100 })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'enum', enum: ['user', 'admin'], default: 'user' })
+  role: string;
+
+  @Column({ nullable: true, comment: 'Code sent for email verification' })
+  verificationCode: string;
+}
